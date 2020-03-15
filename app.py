d642b3b59b74cb674e283ab584c736d53d3b0746
Sundhar <sundhar811@gmail.com>
2020-03-15 08:44:15 +0530

    input validation for add user

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, jsonify, request as req
 from flaskext.mysql import MySQL
 from functools import wraps
+from marshmallow import ValidationError
+
+# Custom Functions
 from db import MySQLConnection
 from random_password_generator import random_password_generator
+from schema import AddUser
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -23,9 +27,15 @@ def auth_wrapper(method):
 
 @app.route('/api/add', methods=['POST'])
 def add_user():
-	data = req.get_json(force = True)
-	print(f'incoming data: {data["type"]}')
-	print(generate_password())
+	try:
+		data = AddUser().load(req.get_json(force = True))
+		print(data)
+	except ValidationError as err:
+		return jsonify(err.messages)
+	except:
+		return jsonify({'message': 'Error occured'})
+	# print(f'incoming data: {data["type"]}')
+	# print(generate_password())
 	return jsonify({ 'status': 200, 'response': 'OK' })	
 
 @app.route('/', methods=['GET'])
--- schema.py
+++ schema.py
@@ -1,5 +1,11 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate
 
 class AddUser(Schema):
-  # type = 
-  pass
+  user_type = fields.String(validate=validate.OneOf(["admin", "manager", "staff"]))
+  first_name = fields.String(validate=validate.Length(max=20))
+  last_name = fields.String(required = True, validate=validate.Length(max=20))
+  Title = fields.String(required = True, validate=validate.Length(max=30))
+  phone_number = fields.String(validate=validate.Length(max=15))
+  date_of_birth = fields.Date(required = True) # ISO Format: YYYY-MM-DD
+  email = fields.Email(required = True)
+  username = fields.String(required = True, validate=validate.Length(max=20))
